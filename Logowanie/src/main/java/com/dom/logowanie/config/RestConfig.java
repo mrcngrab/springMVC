4653cc5c2ff21a0a140abbaa710960622d3034e9
Marcin <mrcngrab@gmail.com>
2016-07-02 13:34:27 +0200

    s

---
+++ Logowanie/src/main/java/com/dom/logowanie/config/RestConfig.java
@@ -0,0 +1,38 @@
+package com.dom.logowanie.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.dom.logowanie.services.UserService;
+import com.dom.logowanie.template.UserJDBCTemplate;
+
+@Configuration
+public class RestConfig {
+
+    @Bean(name="userService")
+    public UserService userService(UserJDBCTemplate userJDBCTemplate) {
+    	UserService userService = new UserService();
+    	userService.setUserJDBCTemplate(userJDBCTemplate);
+        return userService;
+    }
+    
+    @Bean(name="userJDBCTemplate")
+    public UserJDBCTemplate userJDBCTemplate(DataSource dataSource) {
+		UserJDBCTemplate userJDBCTemplate = new UserJDBCTemplate();
+		userJDBCTemplate.setDataSource(dataSource);
+        return userJDBCTemplate;
+    }
+    
+    @Bean(name="dataSource")
+    public DataSource dataSource() {
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    	dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+    	dataSource.setUrl("jdbc:mysql://localhost:3306/Logowanie");
+    	dataSource.setUsername("root");
+    	dataSource.setPassword("ziemniak2");
+		return dataSource;
+    }
+}
